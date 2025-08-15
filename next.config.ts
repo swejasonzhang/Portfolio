6a061393e8dfa748a72ff85e9151eed1573855a3
swejasonzhang <swejasonzhang@gmail.com>
2025-08-15 00:04:37 -0400

    still working on hosting

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export", // enables static site export
-};
+  output: 'export',
+}
 
-export default nextConfig;
+module.exports = nextConfig
